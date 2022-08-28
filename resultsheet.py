8a7499d10d0956d5e670c85c8bca5fd070949ec8
Akmal Anfas <akmalanfas7@gmail.com>
2022-08-28 11:57:29 -0700

    resultsheet

---
+++ resultsheet.py
@@ -0,0 +1,62 @@
+print ("welcome to your results page")
+maths= input("enter your maths marks")
+english= input("enter your english marks")
+science= input("enter your science marks")
+commerce= input("enter your commerce marks")
+history= input("enter your history marks")
+
+if int (maths)>75:
+    print("maths A")
+elif 65<= int (maths)<=74:
+    print("maths B")
+elif 55<= int (maths)<=64:
+    print("maths C")
+elif 35<=int (maths)<=54:
+    print("maths S")
+elif int (maths)<=34:
+    print("maths W")
+
+if int (english)>75:
+    print("english A")
+elif 65<= int (english)<=74:
+    print("english B")
+elif 55<= int (english)<=64:
+    print("english C")
+elif 35<=int (english)<=54:
+    print("english S")
+elif int (english)<=34:
+    print("english W") 
+
+if int (science)>75:
+    print("science A")
+elif 65<= int (science)<=74:
+    print("science B")
+elif 55<= int (science)<=64:
+    print("science C")
+elif 35<=int (science)<=54:
+    print("science S")
+elif int (science)<=34:
+    print("science W") 
+
+if int (commerce)>75:
+    print("commerce A")
+elif 65<= int (commerce)<=74:
+    print("commerce B")
+elif 55<= int (commerce)<=64:
+    print("commerce C")
+elif 35<=int (commerce)<=54:
+    print("commerce S")
+elif int (commerce)<=34:
+    print("commerce W") 
+
+if int (history)>75:
+    print("history A")
+elif 65<= int (history)<=74:
+    print("history B")
+elif 55<= int (history)<=64:
+    print("history C")
+elif 35<=int (history)<=54:
+    print("history S")
+elif int (history)<=34:
+    print("history W") 
+
