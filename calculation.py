21aed934fdd017348ce1e9f21a81fcc9e5f9de0c
Akmal Anfas <akmalanfas7@gmail.com>
2022-08-31 10:16:45 -0700

    measurment

---
+++ calculation.py
@@ -0,0 +1,39 @@
+print("1= add")
+print("2= substraction")
+print("3= division")
+print("4= multipication")
+
+operators=int (input("select a arithmetic number"))
+digit1=int (input("enter your first value"))
+digit2=int (input("enter your second value"))
+
+
+if (operators)==1:
+    print(digit1+digit2)
+elif (operators)==2:
+    print(digit1-digit2)
+elif (operators)==3:
+    print(digit1/digit2)
+elif (operators)==4:
+    print(digit1*digit2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
