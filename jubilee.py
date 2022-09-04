0d57fe9252d1a94117a5ff5c408e5e6d3ae1572f
Akmal Anfas <akmalanfas7@gmail.com>
2022-09-03 20:51:39 -0700

    loops

---
+++ jubilee.py
@@ -0,0 +1,19 @@
+for x in range (1,101):
+   if x==25:
+       print("year 25 is silver jubilee")
+   elif x==40:
+       print("year 40 is ruby jubilee")
+   elif x==50:
+       print("year 50 is golden jubilee")
+   elif x==60:
+       print("year 60 is diamond jubilee")
+   elif x==65:
+       print("year 65 is sapphire jubilee")
+   elif x==70:
+       print("year 70 is platinum jubilee")
+   elif x==100:
+       print("year 100 is centennial jubilee")
+   else: 
+       print ("Year ",x)
+
+   
